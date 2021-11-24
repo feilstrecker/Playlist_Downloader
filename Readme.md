c2b30fd7bdfdbbf5abfd753df42669a35e35e35b
zToso <zToso@DESKTOP-ICV15QV>
2021-11-24 00:25:30 -0300

    Added more informations about the script.

--- Readme.md
+++ Readme.md
@@ -1,1 +1,11 @@
-Olá, este é um pequeno script feito para baixar playlists em python com pequeno esforço.
+    This script download playlists of Youtube.
+
+"""""""""""""""""""""""""""
+""""""" ATTENTION """""""""
+"""""""""""""""""""""""""""
+
+    Pytube is having problems for install with pip, so, i recommend you, for download in oficcial github
+and putting on the path.
+
+
+# requirements: python3 and pytube library 
--- main.py
+++ main.py
@@ -1,17 +1,33 @@
+# Imports
 from pytube import YouTube
 from pytube import Playlist
 import os
 
+# Taking link of playlist
 link = input('Digite o link da playlist: ')
 
-
+# Oppening the playlist and splitting all videos
 playlist = Playlist(link)
+
+# Print the title of playlist
 print(f"Playlist: {playlist.title}")
+
+# This variable is a counter for print how many was downloaded
+counter = 0
+
+# Taking url by url and downloading them
 for url in playlist.video_urls:
+    counter +=1
+    print(f"Downloading ({counter}/{len(playlist)})")
     ys = YouTube(url)
-    print(f'Titulo: {ys.title}')
+    print(f'Title: {ys.title}')
+
+    # Getting audio only
     v = ys.streams.get_audio_only()
-    out_file = v.download()
+
+    # Transforming the mp4 for mp3 and saving in the path 'musics'
+    out_file = v.download(output_path="musics")
     base, ext = os.path.splitext(out_file)
     new_file = base + '.mp3'
     os.rename(out_file, new_file)
+    os.system('cls')
